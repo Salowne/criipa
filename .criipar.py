85e44906e2dae400d9596b5f69c88dad296be0be
Zwdeff <Xdwnff-04x@users.noreply.github.com>
2017-06-22 00:47:05 -0300

    Add files via upload

--- .criipar.py
+++ .criipar.py
@@ -26,13 +26,13 @@ n4 = '\033[94m'
 n5 = '\033[95m'
 n6 = '\033[96m'
 n7 = '\033[96m'
-__Banner__ = '\n Generate custom IPs Numbers _'
+__Banner__ = '\nGenerate custom IPs Numbers _'
 
-__main__ = '\033[92m 1) =\033[32m xxx.xxx.xxx.xxx\n'\
-          +'\033[92m 0) =\033[32m exit\n'
+__main__ = '\033[92m1) =\033[32m xxx.xxx.xxx.xxx\n'\
+          +'\033[92m0) =\033[32m exit\n'
 
 print (n2+' %s\033[m' %(__Banner__))
-print (n2+'\n Author:\033[32m %s\n\033[92m Version:\033[32m %s\n\033[m'\
+print (n2+'\nAuthor:\033[32m %s\n\033[92mVersion:\033[32m %s\n\033[m'\
 %(__author__,__version__))
 
 def case():
@@ -52,7 +52,7 @@ def case():
                          print (n2+' '+str(a)+'.'+str(b)+'.'+str(c)+'.'+str(d)+str(pho)\
                          +'\033[m')
                          if a == o1 and b == o2 and c == o3 and d == o4:
-                            print (n2+'\n\033[32m Completed. IPs saved in file %s\033[m'\
+                            print (n2+'\n\033[32mCompleted. IPs saved in file %s\033[m'\
                             %(arq.name))
                             sleep(2)
                             arq.close()
@@ -61,7 +61,7 @@ def case():
               sleep(2)
               arq = open(input(n2+' Name for file :: _ \033[m')+ '.txt', 'w')
               def retu(s1,s2,s3,s4,arq):
-                  o1 = input(n2+' Maximum octet number 1. 0/255 :: _ \033[m')
+                  o1 = input(n2+'Maximum octet number 1. 0/255 :: _ \033[m')
                   try:
                      o1 = int(o1)
                      if o1 < s1:
@@ -69,9 +69,9 @@ def case():
                               +' The octet.\033[m')
                         retu(s1,s2,s3,s4,arq)
                   except Exception:
-                     print (n1+' Error: Use only integer numbers.\033[m')
+                     print (n1+'Error: Use only integer numbers.\033[m')
                      retu(s1,s2,s3,s4,arq)
-                  o2 = input(n2+' Maximum octet number 2. 0/255 :: _ \033[m')
+                  o2 = input(n2+'Maximum octet number 2. 0/255 :: _ \033[m')
                   try:
                      o2 = int(o2)
                      if o2 < s2:
@@ -79,9 +79,9 @@ def case():
                               +' The octet.\033[m')
                         retu(s1,s2,s3,s4,arq)
                   except Exception:
-                     print (n1+' Error: Use only integer numbers.\033[m')
+                     print (n1+'Error: Use only integer numbers.\033[m')
                      retu(s1,s2,s3,s4,arq)
-                  o3 = input(n2+' Maximum octet number 3. 0/255 :: _ \033[m')
+                  o3 = input(n2+'Maximum octet number 3. 0/255 :: _ \033[m')
                   try:
                      o3 = int(o3)
                      if o3 < s3:
@@ -89,9 +89,9 @@ def case():
                               +' The octet.\033[m')
                         retu(s1,s2,s3,s4,arq)
                   except Exception:
-                     print (n1+' Error: Use only integer numbers.\033[m')
+                     print (n1+'Error: Use only integer numbers.\033[m')
                      retu(s1,s2,s3,s4,arq)
-                  o4 = input(n2+' Maximum octet number 4. 0/255 :: _ \033[m')
+                  o4 = input(n2+'Maximum octet number 4. 0/255 :: _ \033[m')
                   try:
                      o4 = int(o4)
                      if o4 < s4:
@@ -99,49 +99,49 @@ def case():
                               +' The octet.\033[m')
                         retu(s1,s2,s3,s4,arq)
                   except Exception:
-                     print (n1+' Error: Use only integer numbers.\033[m')
+                     print (n1+'Error: Use only integer numbers.\033[m')
                      retu(s1,s2,s3,s4,arq)
-                  pho = input(n2+' Port for IPs :80/:8080 :: _ \033[m')
+                  pho = input(n2+'Port for IPs :80/:8080 :: _ \033[m')
                         
-                  print ('\n\033[32m Generating IPs in file %s .. \n\033[m' %(arq.name))
+                  print ('\n\033[32mGenerating IPs in file %s .. \n\033[m' %(arq.name))
                   sleep(2)
                   gerator(s1,s2,s3,s4,o1,o2,o3,o4,arq,pho)
                   
               def defe(arq):
-                  s1 = input(n2+' IP octet 1. 1/255 :: _ \033[m')
+                  s1 = input(n2+'IP octet 1. 1/255 :: _ \033[m')
                   try:
                      s1 = int(s1)
                      if s1 > 255:
-                        print (n1+' Error: The octet must be less than or equal to 255.')
+                        print (n1+'Error: The octet must be less than or equal to 255.')
                   except Exception:
-                    print (n1+' Error: Use only integer numbers.\033[m')
+                    print (n1+'Error: Use only integer numbers.\033[m')
                     defe(arq)
-                  s2 = input(n2+' IP octet 2. 0/255 :: _ \033[m')
+                  s2 = input(n2+'IP octet 2. 0/255 :: _ \033[m')
                   try:
                      s2 = int(s2)
                      if s2 > 255:
-                        print (n1+' Error: The octet must be less than or equal to 255.')
+                        print (n1+'Error: The octet must be less than or equal to 255.')
                         defe(arq)
                   except Exception:
-                     print (n1+' Error: Use only integer numbers.\033[m')
+                     print (n1+'Error: Use only integer numbers.\033[m')
                      defe(arq)
-                  s3 = input(n2+' IP octet 3. 0/255 :: _ \033[m')
+                  s3 = input(n2+'IP octet 3. 0/255 :: _ \033[m')
                   try:
                      s3 = int(s3)
                      if s3 > 255:
-                        print (n1+' Error: The octet must be less than or equal to 255.')
+                        print (n1+'Error: The octet must be less than or equal to 255.')
                         defe(arq)
                   except Exception:
-                     print (n1+' Error: Use only integer numbers.\033[m')
+                     print (n1+'Error: Use only integer numbers.\033[m')
                      defe(arq)
-                  s4 = input(n2+' IP octet 4. 0/255 :: _ \033[m')
+                  s4 = input(n2+'IP octet 4. 0/255 :: _ \033[m')
                   try:
                      s4 = int(s4)
                      if s4 > 255:
-                        print (n1+' Error: The octet must be less than or equal to 255.')
+                        print (n1+'Error: The octet must be less than or equal to 255.')
                         defe(arq)
                   except Exception:
-                     print (n1+' Error: Use only integer numbers.\033[m')
+                     print (n1+'Error: Use only integer numbers.\033[m')
                      defe(arq)
                   retu(s1,s2,s3,s4,arq)
                   
